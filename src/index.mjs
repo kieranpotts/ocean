9d351898568e281d58f4dbff26368f9a1587da27
Kieran Potts <hello@kieranpotts.com>
2024-08-13 17:11:10 +0100

    fix: copy icon file

--- src/index.mjs
+++ src/index.mjs
@@ -6,11 +6,15 @@ const vs = getTheme({ theme: 'vs', name: 'Ocean Light' })
 
 try {
   await mkdir('dist/themes/', { recursive: true })
+  await mkdir('dist/_/', { recursive: true })
+
   await writeFile('dist/themes/vs.json', JSON.stringify(vs, null, 2))
 
   await copyFile('src/CHANGELOG.md', 'dist/CHANGELOG.md')
   await copyFile('src/README.md', 'dist/README.md')
   await copyFile('src/LICENSE.txt', 'dist/LICENSE.txt')
+
+  await copyFile('src/_/wave.png', 'dist/_/wave.png')
 
   await copyFile('src/package.json', 'dist/package.json')
 } catch (err) {
