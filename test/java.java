c73090ca377daf1d2769c35b4ebd86184cc881b4
Kieran Potts <hello@kieranpotts.com>
2024-08-24 08:36:18 +0100

    maintenance: add syntax highlighting code examples

---
+++ test/java.java
@@ -0,0 +1,38 @@
+abstract class Car
+{
+    /*
+
+    It is not compulsory to have abstract methods in an abstract class. But
+    the abstract methods must *all* be implemented in the subclasses that
+    get instantiated.
+
+    */
+
+    public abstract void drive();
+
+    // public abstract void fly();
+
+    public void park()
+    {
+        System.out.println("Parking");
+    }
+}
+
+class Mondeo extends Car
+{
+    public void drive()
+    {
+        System.out.println("Driving Mondeo");
+    }
+}
+
+public class AbstractClasses {
+    public static void main(String[] args) {
+        /* Abstract classes cannot be instantiated. This will not compile. */
+        //Car car = new Car();
+
+        Car car = new Mondeo();
+        car.drive(); // "Driving Mondeo"
+        car.park(); // "Parking"
+    }
+}
